4f182cb1ad15760305cfd5604dd74fd0d619aa14
PhatLy <ldphat99@gmail.com>
2023-08-18 00:36:03 +0700

    fix province coordinate done

--- components/global/ProvinceCoordinate.tsx
+++ components/global/ProvinceCoordinate.tsx
@@ -5,15 +5,18 @@ import { getProvincesAPI } from '@/lib/getProvince'
 
 const provinceData: string[] = []
 const districtData: { [province: string]: string[] } = {}
-const wardData = {}
+const wardData: { [province: string]: string[] } = {}
 
 
 export default function ProvinceCoordinate() {
 
     const [provinces, setProvinces] = useState<Provinces[]>([])
+    const [firstProvince, setFirstProvince] = useState<string>()
     const [province, setProvince] = useState<string[]>()
     const [district, setDistrict] = useState<string>()
-    const [ward, setWard] = useState()
+    const [wards, setWards] = useState<string[]>([])
+    const [ward, setWard] = useState<string>()
+
 
     useEffect(() => {
         const getProvinceList = async () => {
@@ -24,89 +27,81 @@ export default function ProvinceCoordinate() {
 
                 districtData[province.name].forEach((districtName) => {
                     const matchingDistrict = province.districts.find(d => d.name === districtName);
-                    wardData[districtName] = matchingDistrict.wards.map(ward => ward.name);
+                    if (matchingDistrict) {
+                        wardData[districtName] = matchingDistrict.wards.map((ward) => (ward.name));
+                    }
                 })
             })
 
             setProvinces(res)
             setProvince(districtData[provinceData[0]])
+            setFirstProvince(provinceData[0])
             setDistrict(districtData[provinceData[0]][0])
-            setWard(wardData[districtData[provinceData[0]][0]])
+            setWards(wardData[districtData[provinceData[0]][0]])
+            setWard(wardData[districtData[provinceData[0]][0]][0])
 
         }
         getProvinceList()
 
     }, [])
 
-    // console.log("Check province: ", province);
-    // console.log("Check district: ", district);
-    // console.log("Check ward:", ward);
-
-
-
-
     const handleProvinceChange = (value: string) => {
-        console.log(value);
-
+        // value = Thành phố Hà Nội        
+        setFirstProvince(value)
         setProvince(districtData[value])
         setDistrict(districtData[value][0])
-        setWard(wardData[districtData[value][0]])
+        setWards(wardData[districtData[value][0]])
+        setWard(wardData[districtData[value][0]][0])
     }
+
 
     const handleDistrictChange = (value: string) => {
-        console.log(value)
+        // value = Quận Ba Đình
         setDistrict(value)
-        // console.log(province, district, ward);
-        // setWard(wardData)
+        setWards(wardData[value])
+        setWard(wardData[value][0])
     }
-
-    // console.log(wardData['Quận Ninh Kiều']);
-
 
     const handleWardChange = (value: string) => {
-        console.log(value);
-        // setWard(value)
-        // setWard()
+        setWard(value)
     }
-    console.log(ward);
-    
-    // console.log("Check province: ", province);
-    // console.log("Check district: ", district);
+    // console.log("check province", provinces);
+    // console.log("check provinceData", provinceData);
+    // console.log("check districtData", districtData);
+    // console.log("check wardData", wardData);
+    // console.log("check wardData", wardData[district]);
+    // console.log("check province", province);
+    // // console.log("check district", district);
+    // console.log("check ward", ward);
+    // console.log("check wards", wards);
+    console.log(firstProvince);
 
 
     return (
         <>
-            {province && district ? (
-                <Space wrap>
-                    <Select
-                        defaultValue={provinceData[0]}
-                        style={{ width: 140 }}
-                        onChange={handleProvinceChange}
-                        options={provinceData?.map((province) => ({ label: province, value: province }))}
-                    />
-                    <Select
-                        value={district}
-                        style={{ width: 140 }}
-                        onChange={handleDistrictChange}
-                        options={province?.map((province) => ({ label: province, value: province }))}
-                    />
+            <Space wrap>
+                <Select
+                    // defaultValue={firstProvince}
+                    value={firstProvince}
+                    style={{ width: 180 }}
+                    onChange={handleProvinceChange}
+                    options={provinceData?.map((province) => ({ label: province, value: province }))}
+                />
+                <Select
+                    value={district}
+                    style={{ width: 170 }}
+                    onChange={handleDistrictChange}
+                    options={province?.map((province) => ({ label: province, value: province }))}
+                />
 
-                    <Select
-                        value={ward[0]}
-                        style={{ width: 150 }}
-                        onChange={handleWardChange}
-                        options={ward?.map((ward) => ({ label: ward, value: ward }))}
-                    />
+                <Select
+                    value={ward}
+                    style={{ width: 180 }}
+                    onChange={handleWardChange}
+                    options={wards?.map((ward) => ({ label: ward, value: ward }))}
+                />
 
-                </Space >
-            ) : (
-                <div>
-                    loading
-                </div>
-            )
-            }
-
-
+            </Space >
         </>
     )
 }
