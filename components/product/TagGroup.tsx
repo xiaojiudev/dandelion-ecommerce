d101bc8ad217ee42f8a7ab7ad9f20c7a35b5a6e0
PhatLy <ldphat99@gmail.com>
2023-08-19 11:34:02 +0700

    adding tag group

---
+++ components/product/TagGroup.tsx
@@ -0,0 +1,21 @@
+import { Tag } from 'antd'
+import React from 'react'
+
+export default function TagGroup() {
+
+    return (
+        <div className='flex items-center cursor-pointer'>
+            <Tag color="magenta" >#magenta</Tag>
+            <Tag color="red" >#red</Tag>
+            <Tag color="volcano" >#volcano</Tag>
+            <Tag color="orange" >#orange</Tag>
+            <Tag color="gold" >#gold</Tag>
+            <Tag color="lime" >#lime</Tag>
+            <Tag color="green" >#green</Tag>
+            <Tag color="cyan" >#cyan</Tag>
+            <Tag color="blue" >#blue</Tag>
+            <Tag color="geekblue" >#geekblue</Tag>
+            <Tag color="purple" >#purple</Tag>
+        </div>
+    )
+}
