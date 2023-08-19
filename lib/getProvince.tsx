b9ea387c814faef2652b8dc8f76d135ac28da584
PhatLy <ldphat99@gmail.com>
2023-08-19 11:33:47 +0700

    delete comment

--- lib/getProvince.tsx
+++ lib/getProvince.tsx
@@ -8,32 +8,4 @@ export async function getProvincesAPI(): Promise<Provinces[]> {
 
 }
 
-// const a = [
-//     {
-//         "name": "Thành phố Hà Nội",
-//         "code": 1,
-//         "codename": "thanh_pho_ha_noi",
-//         "division_type": "thành phố trung ương",
-//         "phone_code": 24,
-//         "districts": [
-//             {
-//                 "name": "Quận Ba Đình",
-//                 "code": 1,
-//                 "codename": "quan_ba_dinh",
-//                 "division_type": "quận",
-//                 "short_codename": "ba_dinh",
-//                 "wards": [
-//                     {
-//                         "name": "Phường Phúc Xá",
-//                         "code": 1,
-//                         "codename": "phuong_phuc_xa",
-//                         "division_type": "phường",
-//                         "short_codename": "phuc_xa"
-//                     },...
-//                 ]
-//             },...
-//         ]
-//     },...
-
-// ]
 
