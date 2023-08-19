d0cda7c25f0617e1cdce51067a2d786afa6b8d21
PhatLy <ldphat99@gmail.com>
2023-08-19 11:32:50 +0700

    change default height button size lg,md,sm

--- theme/themeConfig.ts
+++ theme/themeConfig.ts
@@ -6,7 +6,11 @@ const theme: ThemeConfig = {
         fontSize: 14,
         colorPrimary: '#e94b7c',
         colorLinkActive: '#e94b7c',
-        colorLink: '#f06292'
+        colorLink: '#f06292',
+        controlHeightLG: 48,
+        controlHeightSM: 32,
+        controlHeightXS: 24,
+        controlHeight: 42,
     },
 };
 
