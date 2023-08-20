e5ea45cc1d47f6c3f33ad4f21f6036930ba788e8
PhatLy <ldphat99@gmail.com>
2023-08-20 11:16:25 +0700

    restructure

--- app/(Header&Footer)/product/[id]/page.tsx
+++ app/(Header&Footer)/product/[id]/page.tsx
@@ -5,6 +5,8 @@ import ProductDetailInfo from '@/components/product/ProductDetailInfo'
 import ProductDetailSlider from '@/components/product/ProductDetailSlider'
 import TagGroup from '@/components/product/TagGroup'
 import PromoCard from '@/components/product/PromoCard'
+import RatingCustom from '@/components/global/RatingCustom'
+import ProgressRating from '@/components/product/ProgressRating'
 
 
 
@@ -26,7 +28,7 @@ export default async function Page({ params }: { params: { id: string } }) {
 
 
     return (
-        <div>
+        <div className='pb-10'>
             {/* Product section */}
             <div className='bg-white rounded shadow my-10 p-4'>
                 <div className='flex flex-col gap-y-8'>
@@ -42,35 +44,65 @@ export default async function Page({ params }: { params: { id: string } }) {
                 </div>
             </div>
 
-            {/* Product desc section - Review section - Promote section */}
-            <div className='grid gap-4 grid-cols-12 grid-rows-2'>
+            {/* Product desc section - Review section - Promote section etc */}
+            <div className='flex gap-3'>
 
-                {/* Desc section */}
-                <div className='col-span-9 row-span-1'>
-                    <div className='bg-white p-4 rounded'>
-                        <div className='text-slate-800'>
-                            <h1 className='bg-slate-100 p-3 rounded-sm text-lg font-medium'>Product Description</h1>
-                            <div className='flex flex-col gap-4 p-4'>
-                                <p className='text-justify'>
-                                    - Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nobis dicta aut nulla vero perferendis, ipsam labore dolorum veritatis, sed nam excepturi corrupti aliquam enim. Dolorum quam quos est amet.
-                                    • Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ab optio beatae, vero illum pariatur necessitatibus, omnis nisi neque id voluptates adipisci sit voluptate. Incidunt eaque expedita aspernatur alias excepturi!
-                                    • Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus modi, reiciendis officiis, in tenetur fugit nostrum reprehenderit voluptatem inventore quos commodi suscipit exercitationem nesciunt numquam dolore atque totam porro error.
-                                    • Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam deserunt vel praesentium error omnis. Veritatis ab, voluptatum eveniet veniam minima ratione, quas, dignissimos mollitia commodi nostrum neque eum repellat quidem!
-                                </p>
-                                <div className='select-none'>
-                                    <TagGroup />
+                {/* Left section */}
+                <div className='flex-1'>
+                    <div className='flex flex-col gap-3'>
+
+                        {/* Product Description */}
+                        <div className='bg-white p-4 rounded'>
+                            <div className='text-slate-800'>
+                                <h1 className='bg-slate-100 p-3 rounded-sm text-lg font-medium'>Product Description</h1>
+                                <div className='flex flex-col gap-4 p-4'>
+                                    <p className='text-justify'>
+                                        - Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nobis dicta aut nulla vero perferendis, ipsam labore dolorum veritatis, sed nam excepturi corrupti aliquam enim. Dolorum quam quos est amet.
+                                        • Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ab optio beatae, vero illum pariatur necessitatibus, omnis nisi neque id voluptates adipisci sit voluptate. Incidunt eaque expedita aspernatur alias excepturi!
+                                        • Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus modi, reiciendis officiis, in tenetur fugit nostrum reprehenderit voluptatem inventore quos commodi suscipit exercitationem nesciunt numquam dolore atque totam porro error.
+                                        • Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam deserunt vel praesentium error omnis. Veritatis ab, voluptatum eveniet veniam minima ratione, quas, dignissimos mollitia commodi nostrum neque eum repellat quidem!
+                                    </p>
+                                    <div className='select-none'>
+                                        <TagGroup />
+                                    </div>
                                 </div>
                             </div>
                         </div>
+
+                        {/* Review section */}
+                        <div className='bg-white p-4 rounded'>
+                            <div>
+                                <h1 className='bg-slate-100 p-3 rounded-sm text-lg font-medium'>Customer Reviews</h1>
+                                <div className='flex gap-3 p-3'>
+                                    <div className='flex-1 border-r'>Filter group</div>
+                                    <div className='flex flex-col items-center gap-3'>
+                                        <div className='flex gap-3 items-center'>
+                                            <RatingCustom value={4.6} disabled allowHalf className='text-lg' />
+                                            <div className='flex-1 text-base font-medium'>Based on 1624 reviews</div>
+                                        </div>
+                                        <div className='flex flex-col justify-center items-start gap-2 bg-slate-50 p-4 w-[370px] rounded'>
+                                            <ProgressRating rating={5} percent={63} />
+                                            <ProgressRating rating={4} percent={10} />
+                                            <ProgressRating rating={3} percent={6} />
+                                            <ProgressRating rating={2} percent={12} />
+                                            <ProgressRating rating={1} percent={9} />
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                {/* Promote section  */}
-                <div className='col-span-3 row-span-2'>
+                {/* Right section  */}
+                <div className='flex flex-col gap-3'>
+
+                    {/* Promote section */}
                     <div className='bg-white p-4 rounded'>
                         <div>
                             <h2 className='text-sm font-medium font-tilt text-primary-500 mb-3'>Shop Vouchers</h2>
-                            <div className='flex flex-col gap-3 max-h-72 overflow-y-scroll'>
+                            <div className='flex flex-col gap-3 max-h-[295px] overflow-y-scroll'>
                                 <PromoCard />
                                 <PromoCard />
                                 <PromoCard />
@@ -79,14 +111,16 @@ export default async function Page({ params }: { params: { id: string } }) {
                             </div>
                         </div>
                     </div>
+
+                    {/* Another section here */}
+
                 </div>
 
-                {/* Review section */}
-                <div className='col-span-9 row-span-1'>
-                    <div className='bg-white p-4 rounded'>
-                        comment
-                    </div>
-                </div>
+
+
+
+
+
             </div>
         </div>
     )
