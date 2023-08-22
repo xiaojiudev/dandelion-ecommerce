a055a1a2b37aeba5f3f2803c6032689c6216f65f
PhatLy <ldphat99@gmail.com>
2023-08-22 23:17:03 +0700

    change height for promocard

--- components/product/PromoCard.tsx
+++ components/product/PromoCard.tsx
@@ -5,7 +5,7 @@ import ButtonCustom from '../header/ButtonCustom'
 export default function PromoCard() {
     return (
         <>
-            <div className='h-26 w-full flex divide-x divide-slate-200 bg-primary-50 border-zigzag border-y border-r rounded-r-sm'>
+            <div className='h-24 w-full flex divide-x divide-slate-200 bg-primary-50 border-zigzag border-y border-r rounded-r-sm'>
                 <div className='flex flex-col items-start justify-center gap-3 p-3 basis-2/3 text-xs select-none'>
                     <div className='text-primary-500'>
                         <div className='font-medium text-sm'><span>$2</span> off</div>
