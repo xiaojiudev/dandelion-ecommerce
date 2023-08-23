3426e615696ee9a537b67087a817f8c83a4263d9
PhatLy <ldphat99@gmail.com>
2023-08-23 12:14:00 +0700

    preview img-video features complete!

--- components/product/MultiImagePreview.tsx
+++ components/product/MultiImagePreview.tsx
@@ -1,50 +1,105 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Image } from 'antd'
 
 export default function MultiImagePreview() {
+
+    const sourceList = [
+        {
+            type: 'img',
+            src: '/avatar.jpg',
+            sizeSmall: 70,
+            sizeZoom: 500
+        },
+        {
+            type: 'img',
+            src: '/avatar1.jpg',
+            sizeSmall: 70,
+            sizeZoom: 500
+        },
+        {
+            type: 'video',
+            src: '/1.mp4',
+            thumb: '/thumbnail.png',
+            sizeSmall: 70,
+            sizeZoom: 500
+        },
+
+    ]
+
+    const previewList = sourceList.map((item, index) => {
+        if (item.type === 'img') {
+            return (
+                <Image
+                    width={item.sizeZoom}
+                    height={item.sizeZoom}
+                    src={item.src}
+                    className='object-cover'
+                    preview={false}
+                />
+            )
+        }
+
+        if (item.type === 'video') {
+            return (
+                <video
+                    controls
+                    muted
+                    autoPlay
+                    className='w-[560px] h-[315px] object-contain'
+                >
+                    <source src={item.src} type="video/mp4" />
+                </video>
+            )
+        }
+
+    })
+
+    const thumbList = sourceList.map((item, index) => (
+        <Image
+            key={index}
+            width={item.sizeSmall}
+            height={item.sizeSmall}
+            src={item.thumb ?? item.src}
+            className='object-cover rounded-sm'
+        />
+    ))
+
     return (
         <div className='flex items-center gap-3'>
             <Image.PreviewGroup
                 preview={{
                     onChange: (current, prev) => console.log(`current index: ${current}, prev index: ${prev}`),
-                }}
+                    imageRender: (originalNode, info) => {
+                        // console.log(info.current)
 
+                        return (previewList[info.current])
+                    },
+                    destroyOnClose: true,
+
+                }}
             >
-                <Image
+                {/* <Image
                     width={70}
                     height={70}
                     src="/avatar.jpg"
-                    className='object-cover'
+                    className='object-cover rounded-sm'
                 />
                 <Image
                     width={70}
                     height={70}
                     src="/avatar1.jpg"
-                    className='object-cover'
+                    className='object-cover rounded-sm'
                 />
                 <Image
                     width={70}
                     height={70}
-                    preview={{
-                        imageRender: () => (
-                            <iframe
-                                width="560"
-                                height="315"
-                                src="https://www.youtube.com/embed/dzocv-uSYDk"
-                                title="YouTube video player"
-                                className='border-0'
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                allowFullScreen>
-                            </iframe>
-                        ),
-                        toolbarRender: () => null,
-                    }}
-                    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-                />
+                    src="/thumbnail.png"
+                    className='object-cover rounded-sm'
+                /> */}
+                {thumbList}
 
-                
             </Image.PreviewGroup>
 
         </div>
