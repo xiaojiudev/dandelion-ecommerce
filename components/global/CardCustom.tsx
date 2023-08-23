36825d281d3304c5ebe68b4fa22f6eea01af0027
PhatLy <ldphat99@gmail.com>
2023-08-23 12:12:52 +0700

    add props loading skeleton

--- components/global/CardCustom.tsx
+++ components/global/CardCustom.tsx
@@ -11,10 +11,11 @@ type CardProps = {
     cardTitle?: string,
     cardPrice?: number,
     productSold?: number
+    loading?: boolean
 }
 
 
-export default function CardCustom({ href, sale, imgSrc, cardTitle, cardPrice, productSold, ...props }: CardProps) {
+export default function CardCustom({ href, sale, imgSrc, cardTitle, cardPrice, productSold, loading = false, ...props }: CardProps) {
 
     const [color, setColor] = useState({ active: false, fill: 'rgba(0, 0, 0, 0.5)', stroke: '#fff' })
 
@@ -30,8 +31,7 @@ export default function CardCustom({ href, sale, imgSrc, cardTitle, cardPrice, p
         }
     }
 
-    const [loading, setLoading] = useState(false)
-
+    // const [loading, setLoading] = useState(false)
 
     return (
         <>
@@ -61,24 +61,26 @@ export default function CardCustom({ href, sale, imgSrc, cardTitle, cardPrice, p
                                 <div className='text-xs text-[#0000008a]'>2.6k sold</div>
                             </div>
                         </div>
+                        
+                        {/* Button */}
+                        <ConfigProvider theme={{
+                            token: {
+                                colorPrimary: '#e8dfda'
+                            },
+                        }}>
+                            <Button
+                                className="flex flex-col-reverse mb-1 mr-4 cursor-pointer bg-[#e8dfda] w-9 h-9 rounded-full items-center justify-center absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2"
+                                onClick={(e) => { e.preventDefault() }}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6  opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                                </svg>
+                            </Button>
+                        </ConfigProvider>
                     </>
                 )}
 
-                {/* Button */}
-                <ConfigProvider theme={{
-                    token: {
-                        colorPrimary: '#e8dfda'
-                    },
-                }}>
-                    <Button
-                        className="flex flex-col-reverse mb-1 mr-4 cursor-pointer bg-[#e8dfda] w-9 h-9 rounded-full items-center justify-center absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2"
-                        onClick={(e) => { e.preventDefault() }}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6  opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                        </svg>
-                    </Button>
-                </ConfigProvider>
+
             </a>
 
         </>
