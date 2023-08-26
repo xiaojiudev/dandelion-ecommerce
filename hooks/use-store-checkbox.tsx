034698d9b12ecf39bfd3c202c35a39e3c7b82b6a
PhatLy <ldphat99@gmail.com>
2023-08-26 22:48:56 +0700

    remove useStore zustand

--- hooks/use-store-checkbox.tsx
+++ hooks/use-store-checkbox.tsx
@@ -1,14 +1,2 @@
 import { create } from 'zustand'
 
-interface useStoreCheckboxStore {
-    isCheckedAll: boolean
-    checkedAll: () => void
-    uncheckedAll: () => void
-}
-
-export const useStoreCheckbox = create<useStoreCheckboxStore>((set) => ({
-    isCheckedAll: false,
-    checkedAll: () => set(() => ({ isCheckedAll: true })),
-    uncheckedAll: () => set(() => ({ isCheckedAll: false })),
-}))
-
