de8802105db9369d72de9e6e882ce869b54df00f
PhatLy <ldphat99@gmail.com>
2023-08-26 22:49:42 +0700

    handle checkbox, delete cart

--- app/(root)/checkout/cart/page.tsx
+++ app/(root)/checkout/cart/page.tsx
@@ -1,11 +1,22 @@
 'use client'
-import CartItem from '@/components/checkout/CartItem';
-import { Checkbox } from 'antd'
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image';
+import { Button, Checkbox, ConfigProvider, Empty, InputNumber, Modal, Skeleton, message } from 'antd'
 import { CheckboxChangeEvent } from 'antd/es/checkbox';
 import { Trash2 } from 'lucide-react';
-import React, { useEffect, useState } from 'react'
 
-import { useStoreCheckbox } from '@/hooks/use-store-checkbox'
+import CartItem from '@/components/checkout/CartItem';
+
+type userCartDataProps = {
+    id: string,
+    image: string,
+    name: string,
+    type: string,
+    unitPrice: number,
+    maxQuantity: number,
+    totalPrice: number,
+    isChecked?: boolean
+}
 
 const userCartData = [
     {
@@ -37,28 +48,66 @@ const userCartData = [
     },
 ]
 
+
 export default function Checkout() {
 
-    const [cartItem, setCartItem] = useState<any>([])
+    const [carts, setCarts] = useState<userCartDataProps[]>([])
+    const [loading, setLoading] = useState(true)
+    const [openModal, setOpenModal] = useState(false)
 
-    useEffect(() => {
-        setCartItem(userCartData)
-    }, [])
+    const [messageApi, contextHolder] = message.useMessage()
 
-    const bears = useStoreCheckbox((state) => state.isCheckedAll)
+    const warning = () => {
 
-    const checkedAll = useStoreCheckbox((state) => state.checkedAll)
-    const uncheckedAll = useStoreCheckbox((state) => state.uncheckedAll)
-
-    const onChange = (e: CheckboxChangeEvent) => {
-        // console.log(`checked = ${e.target.checked}`)
-        const checked = e.target.checked
-
-        checked ? checkedAll() : uncheckedAll()
+        messageApi.open({
+            type: 'warning',
+            content: 'Please select the product to delete',
+        })
 
     }
-    // console.log(bears);
 
+    useEffect(() => {
+        setCarts(userCartData)
+        setLoading(false)
+    }, [])
+
+
+    const handleChange = (e: CheckboxChangeEvent) => {
+        const { name, checked } = e.target
+
+        if (name === 'allSelect') {
+            let temCart = carts.map(cart => { return { ...cart, isChecked: checked } })
+            setCarts(temCart)
+        } else {
+            let tempCart = carts.map(cart => cart.id === name ? { ...cart, isChecked: checked } : cart)
+            setCarts(tempCart)
+        }
+
+    }
+
+    const handleDeleteCart = (id: string) => {
+        if (id === 'allSelect') {
+
+        } else {
+            let temCart = carts.filter(cart => cart.id !== id)
+            setCarts(temCart)
+        }
+    }
+
+    const isCheckedAll = !loading && carts.length > 0 && carts.filter(cart => cart?.isChecked === true).length === carts.length
+
+    const showModal = (isCheckedAll: boolean) => {
+        console.log(isCheckedAll);
+        if (isCheckedAll) {
+            setOpenModal(true);
+        } else {
+            warning()
+        }
+    }
+
+    const handleCancel = () => {
+        setOpenModal(false)
+    }
 
     return (
         <div className='my-10'>
@@ -67,20 +116,92 @@ export default function Checkout() {
                     <div className="col-span-9">
                         <div className='flex flex-col flex-1 justify-center gap-3'>
                             <div className='bg-white p-4 rounded shadow-sm'>
-                                <div className='grid grid-cols-12'>
-                                    <Checkbox onChange={onChange} className='col-span-5'>Alll Product (10 products) </Checkbox>
+                                <div className='grid grid-cols-12 select-none'>
+
+                                    {/* Checkbox for Selected all */}
+                                    <Checkbox
+                                        className='col-span-5'
+                                        name='allSelect'
+                                        onChange={handleChange}
+                                        checked={isCheckedAll}
+                                    >
+                                        Alll Product ({carts.length} products)
+                                    </Checkbox>
                                     <span className='col-span-2'>Unit Price</span>
                                     <span className='col-span-2'>Quantity</span>
                                     <span className='col-span-2'>Total Price</span>
                                     <button className='col-span-1 flex items-center'>
-                                        <Trash2 size={16} strokeWidth={2} color='' className='stroke-slate-600' />
+                                        <Trash2 onClick={() => showModal(isCheckedAll)} size={16} strokeWidth={2} color='' className='stroke-slate-600' />
+                                        <Modal
+                                            title="Title"
+                                            open={openModal}
+                                            // onOk={handleOk}
+                                            // confirmLoading={confirmLoading}
+                                            onCancel={handleCancel}
+                                        >
+                                            <p>{'modalText'}</p>
+                                        </Modal>
+                                        <ConfigProvider
+
+                                        >   
+                                            { contextHolder}
+                                        </ConfigProvider>
                                     </button>
                                 </div>
                             </div>
 
-                            {/* <CartItem /> */}
-                            {cartItem.map((item: any, index: number) => (
-                                <CartItem id={item.id} image={item.image} name={item.name} type={item.type} unitPrice={item.unitPrice} totalPrice={item.totalPrice} maxQuantity={item.maxQuantity} key={index} />
+                            {/*Cart Skeleton */}
+                            {loading && (<>
+                                <section className='bg-white p-4 rounded shadow-sm select-none'>
+                                    <div className='flex flex-row items-center gap-3'>
+                                        <Skeleton.Image active className='h-20 w-20' />
+                                        <Skeleton active paragraph={{ rows: 1, width: '100%' }} round title={{ width: '100%' }} />
+                                    </div>
+                                </section>
+                            </>)}
+
+                            {/* User Cart = 0 */}
+                            {!loading && carts.length === 0 && (
+                                <div className='flex flex-1 items-center justify-center p-4 bg-white shadow'>
+                                    <Empty
+                                        description={
+                                            <span className='text-base'>There are no products in your shopping cart.</span>
+                                        }
+                                    >
+                                        <Button type='primary' size='middle' href='/product'>Continue Shopping</Button>
+                                    </Empty>
+                                </div>
+                            )}
+
+                            {/* User Cart Item */}
+                            {carts.map((cart: userCartDataProps) => (
+                                <section className='bg-white p-4 rounded shadow select-none' key={cart.id}>
+                                    <div className='grid grid-cols-12 items-center'>
+                                        <Checkbox
+                                            className='col-span-5'
+                                            name={cart.id}
+                                            onChange={handleChange}
+                                            checked={cart?.isChecked || false}
+                                        >
+                                            <div className='flex items-center gap-3'>
+                                                <Image src={cart.image} alt='item' sizes="100vw" priority quality={100} width={500} height={300} className='w-20 h-20 object-cover rounded-sm' />
+                                                <div className='flex flex-col items-start gap-2'>
+                                                    <h1 className='text-base font-medium text-gray-800 line-clamp-1'>{cart.name}</h1>
+                                                    <p className='text-sm font-normal text-gray-600 line-clamp-1'>{cart.type}</p>
+                                                </div>
+                                            </div>
+                                        </Checkbox>
+                                        <span className='col-span-2 '>${cart.unitPrice}</span>
+                                        <span className='col-span-2 '>
+                                            <InputNumber min={1} max={cart.maxQuantity} defaultValue={1} size='small' />
+                                        </span>
+                                        <span className='col-span-2 '>${cart.totalPrice}</span>
+                                        <button className='col-span-1 '>
+                                            <Trash2 onClick={() => handleDeleteCart(cart.id)} size={16} strokeWidth={2} color='' className='stroke-slate-600' />
+                                        </button>
+
+                                    </div>
+                                </section>
                             ))}
                         </div>
                     </div>
