5a277fd6c6ca70f828d07d140712a692e535d418
PhatLy <ldphat99@gmail.com>
2023-08-31 22:26:56 +0700

    add bg-img src

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,7 @@ module.exports = {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic':
                     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+                'img-confetti': "url('/assets/img/confetti.svg')",
             },
         },
         fontFamily: {
