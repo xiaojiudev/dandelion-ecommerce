15843533f7f2ef90048e70ad9eaaeb8b1ccd736e
PhatLy <ldphat99@gmail.com>
2023-09-03 11:28:38 +0700

    sign in page

--- app/(auth)/(routes)/session/signin/page.tsx
+++ app/(auth)/(routes)/session/signin/page.tsx
@@ -1,7 +1,68 @@
+import LogoSVG from '@/components/global/LogoSVG'
 import React from 'react'
 
 export default function Signin() {
-  return (
-    <div>Signin page</div>
-  )
+    return (
+        <div className='flex flex-row items- h-full bg-white'>
+            <section className='w-[450px]'>
+                <div className='flex flex-col h-full text-white'>
+                    <a href="/" className='absolute z-10 top-10 left-10'>
+                        <span className="self-center text-2xl font-dancing-script font-bold whitespace-nowrap tracking-wide">Dandelion</span>
+                    </a>
+                    {/* <video playsInline className="flex w-full h-full object-cover" autoPlay loop muted src="https://cdn.dribbble.com/uploads/48226/original/b8bd4e4273cceae2889d9d259b04f732.mp4?1689028949">
+                    </video> */}
+                    <video playsInline className="flex w-full h-full object-cover" autoPlay loop muted src="/signin.mp4">
+                    </video>
+                    {/* <img src="https://cdn.dribbble.com/users/4835348/screenshots/17800300/media/083a731a314fde97c890a8f026173661.png" alt="" className='w-full h-full object-cover' /> */}
+                    
+                    <a className="absolute bottom-10 left-10 z-10" href="https://www.facebook.com/pugongying999">
+                        @xiaojiu
+                    </a>
+                </div>
+            </section>
+            <section className='flex-1'>
+                <div className="bg-gray-50 dark:bg-gray-900">
+                    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                            <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+                            Flowbite
+                        </a>
+                        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                    Create and account
+                                </h1>
+                                <form className="space-y-4 md:space-y-6" action="#">
+                                    <div>
+                                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                        <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                                        <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                    </div>
+                                    <div className="flex items-start">
+                                        <div className="flex items-center h-5">
+                                            <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
+                                        </div>
+                                        <div className="ml-3 text-sm">
+                                            <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                                        </div>
+                                    </div>
+                                    <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                        Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                                    </p>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 }
