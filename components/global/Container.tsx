9d850b8b9e6807a11dda8461ecf619f6692d7a38
PhatLy <ldphat99@gmail.com>
2023-09-09 20:54:12 +0700

    add margin y for Container

--- components/global/Container.tsx
+++ components/global/Container.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Container({ children }: { children: React.ReactNode }) {
     return (
-        <main className='container max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <main className='container max-w-screen-xl mx-auto my-10 px-4 sm:px-6 lg:px-8'>
             {children}
         </main>
     )
