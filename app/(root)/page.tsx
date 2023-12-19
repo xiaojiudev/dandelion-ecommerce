afcb2ff65056b164f380e8ddbf6555b373d2d4d7
ldphat99 <ldphat99@gmail.com>
2023-12-19 22:22:31 +0700

    UPDATE: refactor with Suspense

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,17 @@
 import Image from 'next/image'
 import React, { Suspense } from 'react'
 
-import SliderCustom from '@/components/global/SliderCustom'
 import CarouselWrapper from '@/components/home/CarouselWrapper'
 import TitleTypography from '@/components/global/TitleTypography'
 import FilterSortByGroup from '@/components/home/FilterSortByGroup'
-import ProductCard from '@/components/global/ProductCard'
-import MyPagination from '@/components/global/MyPagination'
-import MyCategoryGroup from '@/components/home/MyCategoryGroup'
+import { List } from 'lucide-react'
+import { CategoriesSkelton } from '@/components/skeletons'
+import CategoryBar from '@/components/home/CategoryBar'
+import ProductWrapper from '@/components/home/ProductWrapper'
 
 
 export default async function Home({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
     const { page = 0, size = 10, sortBy = '', sortDir = '', category = '' } = searchParams;
-
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URI}/products?page=${page}&size=${size}&sortBy=${sortBy}&sortDir=${sortDir}&category=${category}`, { next: { revalidate: 0 } });
-    const data = await res.json();
-    const { content, pageNumber, pageSize, totalElements, totalPages, lastPage } = data?.products;
-    console.log(pageNumber);
-
-    console.log(searchParams);
-
 
 
     return (
@@ -38,12 +30,12 @@ export default async function Home({ searchParams }: { searchParams: { [key: str
                 </div>
 
                 {/* Slider Popular Product */}
-                <div id='popular'>
+                {/* <div id='popular'>
                     <div className='p-4 py-10 shadow-none flex flex-col justify-center'>
                         <TitleTypography headingText='Seller Spotlight' descText='Unveiling Premier Selections from Trusted Partners' />
                         <SliderCustom />
                     </div>
-                </div>
+                </div> */}
 
                 {/* Main product */}
                 <div id='product' className='p-4 py-10 w-full bg-white shadow-none rounded flex flex-col justify-center'>
@@ -53,20 +45,31 @@ export default async function Home({ searchParams }: { searchParams: { [key: str
                     </div>
                     <div className='grid grid-cols-6 gap-4'>
                         <div className='col-span-1'>
-                            <MyCategoryGroup />
+                            <div className="max-w-xs flex flex-col rounded-lg bg-white shadow">
+                                <div className="inline-flex items-center border-b gap-x-3.5 py-3 px-4 text-base font-semibold text-primary-600 -mt-px  first:mt-0  focus:z-10 focus:outline-none select-none">
+                                    <List size={16} />
+                                    Category
+                                </div>
+                                <Suspense fallback={<CategoriesSkelton/>}>
+                                    <CategoryBar/>
+                                </Suspense>
+                            </div>
                         </div>
                         <div className='col-span-5'>
-                            {/* <ProductsPagination /> */}
+                            {/* <Products/> */}
                             <div className='flex flex-col items-center'>
                                 <div className='w-full h-full mb-10 grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 sm:gap-y-8 md:gap-7 lg:gap-6 xl:gap-x-4 xl:gap-y-6 justify-items-center'>
-                                    <ProductCard data={content} />
+                                    {/* <ProductCard data={content} /> */}
+                                    <Suspense fallback={<div>Loading...</div>}>
+                                        <ProductWrapper/>
+                                    </Suspense>
                                 </div>
                             </div>
                         </div>
                         <div className='col-span-1'></div>
                         <div className='col-span-5'>
                             <div className='flex justify-center'>
-                                <MyPagination pageNumber={pageNumber + 1} totalElements={totalElements} />
+                                {/* <MyPagination pageNumber={pageNumber + 1} totalElements={totalElements} /> */}
                             </div>
                         </div>
                     </div>
