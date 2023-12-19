158d5a1a51d6555e871520265d47b1a539ddc8f7
ldphat99 <ldphat99@gmail.com>
2023-12-19 22:24:45 +0700

    ADD: lib for fetching data

---
+++ lib/data.ts
@@ -0,0 +1,28 @@
+
+export async function fetchProducts(page = 0, size = 10, sortBy = '', sortDir = '', category = 'All') {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const res = await fetch(`${process.env.BACKEND_PUBLIC_API_URI}/products?page=${page}&size=${size}&sortBy=${sortBy}&sortDir=${sortDir}&category=${category}`, {
+        next: {
+            revalidate: 0,
+        }
+    });
+
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+}
+
+export async function fetchCategories() {
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    const res = await fetch('http://localhost:8080/api/v1/categories');
+
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+}
