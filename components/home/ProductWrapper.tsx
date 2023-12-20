b51e2c316bb87a00b32841aed410a0078e9f25b5
ldphat99 <ldphat99@gmail.com>
2023-12-20 10:32:09 +0700

    UPDATE: product props

--- components/home/ProductWrapper.tsx
+++ components/home/ProductWrapper.tsx
@@ -4,20 +4,33 @@ import Image from 'next/image';
 
 import { Product } from '@/types/types';
 import { fetchProducts } from '@/lib/data';
-import HeartActive from '@/components/global/HeartActive';
+import HeartFavorite from '@/components/global/HeartFavorite';
 import ProductCart from '../global/ProductCart';
 
-export default async function ProductWrapper({ page, size = 10, category }: { page: number, size?: number, category: string }) {
+export default async function ProductWrapper({
+    page = 0,
+    size = 10,
+    sortBy = '',
+    sortDir = '',
+    category = '',
+}: {
+    page: number,
+    size?: number,
+    sortBy?: string,
+    sortDir?: string,
+    category?: string,
+}) {
 
     const options = {
         page,
         size,
-        sortBy: undefined,
-        sortDir: undefined,
+        sortBy,
+        sortDir,
         category,
     }
 
     const res = await fetchProducts({ ...options });
+
     const { content } = res;
 
     return res && (
@@ -35,8 +48,8 @@ export function ProductCard({ data }: { data: Product[] }) {
             className="relative min-w-[180px] max-w-[200px] h-[285px] block bg-white text-gray-900 shadow-md rounded-2xl p-2 cursor-pointer hover:-translate-y-1 transition-all select-none">
             <div className="rounded-md relative overflow-hidden">
                 <span className="absolute left-2 top-2 mt-2 z-10 rounded-full bg-primary-700 px-2 text-center text-sm font-medium text-white select-none">-39%</span>
-                <Image src={`${product?.media_url}`} alt={`${product?.name}`} priority quality={100} sizes="100vw" width={500} height={300} className='h-44 rounded-sm w-full object-cover hover:scale-105 transition duration-500' />
-                <HeartActive id={product?.id} className='absolute right-2 top-2' />
+                <Image src={`${product?.media_url}`} alt={`${product?.name}`} quality={70} sizes="100vw" width={500} height={300} className='h-44 rounded-sm w-full object-cover hover:scale-105 transition duration-500' />
+                <HeartFavorite id={product?.id} className='absolute right-2 top-2' />
             </div>
             <div className="my-4 pl-2 mb-2 flex flex-col justify-between items-stretch">
                 <div className=''>
