8e43a599cea93b30a11e0747a4a21e081b64e87f
ldphat99 <ldphat99@gmail.com>
2023-12-20 10:29:53 +0700

    ADD: handle pagination

---
+++ components/global/PaginationWrapper.tsx
@@ -0,0 +1,29 @@
+'use client'
+import React from 'react'
+import { Pagination } from 'antd'
+import type { PaginationProps } from 'antd';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+
+export default function PaginationWrapper({ pageNumber, totalElements }: { pageNumber: number, totalElements: number }) {
+
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+    const onChange: PaginationProps['onChange'] = (pageValue: number) => {
+        const params = new URLSearchParams(searchParams);
+        if (pageValue) {
+            params.set('page', (pageValue - 1).toString());
+        } else {
+            params.delete('page');
+        }
+
+        replace(`${pathname}?${params.toString()}`, { scroll: false });
+    };
+
+    const currentPageNumber = pageNumber >= 0 ? (pageNumber + 1) : 1;
+    
+    return (
+        <Pagination defaultCurrent={1} current={currentPageNumber} total={totalElements} onChange={onChange} />
+    )
+}
