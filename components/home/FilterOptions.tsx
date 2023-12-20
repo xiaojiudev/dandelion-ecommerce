c187aadeb4632c51ca208a95b5d495b482aad8b5
ldphat99 <ldphat99@gmail.com>
2023-12-20 23:01:06 +0700

    UPDATE: handle search products

--- components/home/FilterOptions.tsx
+++ components/home/FilterOptions.tsx
@@ -1,11 +1,10 @@
 'use client'
 import React from 'react'
-import { ConfigProvider, Select, Space } from 'antd'
-
-import SearchInput from '../header/SearchInput'
+import { ConfigProvider, Select, Input } from 'antd';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { SearchProps } from 'antd/es/input';
 
-
+const { Search } = Input;
 
 export default function FilterOptions() {
 
@@ -13,14 +12,31 @@ export default function FilterOptions() {
     const pathname = usePathname();
     const { replace } = useRouter();
 
+    const isSearching = !!searchParams.get('search');
+
     const handleChangeFilter = (value: string) => {
         console.log(`selected ${value}`)
     }
+    
 
-    const handleChangeSort = (optValue: { title: string; value: string; } | undefined) => {
-        // console.log(optValue);
+    const handleSearchProduct: SearchProps['onSearch'] = (value, _e, info) => {
         
-        const sortBy = optValue?.title.split("_")[0].toLowerCase();
+        const params = new URLSearchParams(searchParams);
+        params.set('page', '0');
+
+        if (value.length > 0 && info?.source === 'input') {
+            params.set('search', value.toString());
+            replace(`${pathname}?${params.toString()}`, { scroll: false });
+        } else if (info?.source === 'clear' && isSearching) {
+            params.delete('search');
+            replace(`${pathname}?${params.toString()}`, { scroll: false });
+        }
+    };
+
+    const handleChangeSort = (optValue: { value: string; } | undefined) => {
+        // console.log(optValue);
+
+        const sortBy = optValue?.value.split("_")[0].toLowerCase();
         const sortDir = optValue?.value.split("_")[1].toLowerCase();
 
         const params = new URLSearchParams(searchParams);
@@ -37,61 +53,66 @@ export default function FilterOptions() {
     };
 
     return (
-        <div>
-            <Space wrap>
-                <SearchInput />
+        <div className='flex flex-row justify-between items-center min-w-[500px] max-w-[550px]'>
+            <ConfigProvider theme={{
+                token: {
+                    colorBorder: '#fce7ef',
+                    colorPrimary: '#e94b7c',
+                },
+            }}>
+                <Search
+                    placeholder="Type to search"
+                    onSearch={handleSearchProduct}
+                    style={{ width: 200 }}
+                    size='small'
+                    enterButton
+                    allowClear
+                    maxLength={100}
 
-                <ConfigProvider theme={{
-                    token: {
-                        colorPrimary: '#f8a9c5',
-                        colorBorder: '#fce7ef'
-                    },
-                }}>
-                    <Space wrap>
-                        <Select
-                            placeholder={<div className='text-black'>Filter</div>}
-                            style={{ width: 120 }}
-                            size='small'
-                            onChange={handleChangeFilter}
-                            options={[
-                                { value: 'popular', label: 'Popular' },
-                                { value: 'latest', label: 'Latest' },
-                                { value: 'topsale', label: 'Top Sales' },
+                />
 
-                            ]}
-                            allowClear
-                            placement='bottomLeft'
-                        />
-                        <Select
-                            placeholder={<div className='text-black'>Sort by</div>}
-                            style={{ width: 140 }}
-                            size='small'
-                            onChange={handleChangeSort}
-                            labelInValue
-                            options={[
-                                {
-                                    key: 'Name',
-                                    label: 'Name',
-                                    options: [
-                                        { key: 'name_asc', label: 'A → Z', title: 'name_asc', value: 'name_asc', },
-                                        { key: 'name_desc', label: 'Z → A', title: 'name_desc', value: 'name_desc', },
-                                    ],
-                                },
-                                {
-                                    key: 'Price',
-                                    label: 'Price',
-                                    options: [
-                                        { key: 'price_desc', label: 'High → Low', title: 'price', value: 'price_desc', },
-                                        { key: 'price_asc', label: 'Low → High', title: 'price', value: 'price_asc', },
-                                    ],
-                                },
-                            ]}
-                            allowClear
-                            placement='bottomLeft'
-                        />
-                    </Space>
-                </ConfigProvider>
-            </Space>
+                <Select
+                    placeholder={<div className='text-black'>Filter</div>}
+                    style={{ width: 120 }}
+                    size='small'
+                    onChange={handleChangeFilter}
+                    options={[
+                        { value: 'popular', label: 'Popular' },
+                        { value: 'latest', label: 'Latest' },
+                        { value: 'topsale', label: 'Top Sales' },
+
+                    ]}
+                    allowClear
+                    placement='bottomLeft'
+                />
+                <Select
+                    placeholder={<div className='text-black'>Sort by</div>}
+                    style={{ width: 140 }}
+                    size='small'
+                    onChange={handleChangeSort}
+                    labelInValue
+                    options={[
+                        {
+                            key: 'Name',
+                            label: 'Name',
+                            options: [
+                                { key: 'name_asc', label: 'A → Z', title: 'name_asc', value: 'name_asc', },
+                                { key: 'name_desc', label: 'Z → A', title: 'name_desc', value: 'name_desc', },
+                            ],
+                        },
+                        {
+                            key: 'Price',
+                            label: 'Price',
+                            options: [
+                                { key: 'price_desc', label: 'High → Low', title: 'price', value: 'price_desc', },
+                                { key: 'price_asc', label: 'Low → High', title: 'price', value: 'price_asc', },
+                            ],
+                        },
+                    ]}
+                    allowClear
+                    placement='bottomLeft'
+                />
+            </ConfigProvider>
         </div>
     )
 }
