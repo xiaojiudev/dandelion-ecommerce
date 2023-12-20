45162e7820f66abec94d7ab6ae93e77e056ad8ff
ldphat99 <ldphat99@gmail.com>
2023-12-20 23:02:56 +0700

    UPDATE: add priority into Image and move grid wrapper to ProductWrapper

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import React, { Suspense } from 'react'
 
 import CarouselWrapper from '@/components/home/CarouselWrapper'
 import TitleTypography from '@/components/global/TitleTypography'
-import FilterSortByGroup from '@/components/home/FilterSortByGroup'
+import FilterOptions from '@/components/home/FilterOptions'
 import { List } from 'lucide-react'
 import { CategoriesSkelton, ProductsCardSkeleton } from '@/components/skeletons'
 import CategoryBar from '@/components/home/CategoryBar'
@@ -13,9 +13,9 @@ import { fetchProducts } from '@/lib/data'
 
 
 export default async function Home({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
-    const { page = 0, size = 10, sortBy = '', sortDir = '', category = '' } = searchParams;
+    const { page = 0, size = 10, sortBy = '', sortDir = '', category = '', search } = searchParams;
 
-    const { pageNumber, totalElements } = await fetchProducts({...searchParams});
+    const { pageNumber, totalElements } = await fetchProducts({ ...searchParams });
 
     return (
         <>
@@ -25,10 +25,10 @@ export default async function Home({ searchParams }: { searchParams: { [key: str
                         <CarouselWrapper />
                     </div>
                     <div className='col-span-1 row-span-1 rounded-sm overflow-hidden'>
-                        <Image src={"/avatar.jpg"} alt="avatar" sizes="100vw" quality={70} width={500} height={300} className='object-cover h-full w-full' />
+                        <Image src={"/avatar.jpg"} alt="avatar" sizes="100vw" priority quality={70} width={500} height={300} className='object-cover h-full w-full' />
                     </div>
                     <div className='col-span-1 row-span-1 rounded-sm overflow-hidden'>
-                        <Image src={"/avatar.jpg"} alt="avatar" sizes="100vw" quality={70} width={500} height={300} className='object-cover h-full w-full' />
+                        <Image src={"/avatar.jpg"} alt="avatar" sizes="100vw" priority quality={70} width={500} height={300} className='object-cover h-full w-full' />
                     </div>
                 </div>
 
@@ -44,7 +44,7 @@ export default async function Home({ searchParams }: { searchParams: { [key: str
                 <div id='product' className='p-4 py-10 w-full bg-white shadow-none rounded flex flex-col justify-center'>
                     <TitleTypography classWrapper='mb-4' headingText='Featured Finds' descText='Curated Selections for Your Shopping Delight' />
                     <div className='flex items-center justify-end mb-8'>
-                        <FilterSortByGroup />
+                        <FilterOptions />
                     </div>
                     <div className='grid grid-cols-6 gap-4'>
                         <div className='col-span-1'>
@@ -61,11 +61,9 @@ export default async function Home({ searchParams }: { searchParams: { [key: str
                         <div className='col-span-5'>
                             {/* <Products/> */}
                             <div className='flex flex-col items-center'>
-                                <div className='w-full h-full mb-10 grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 sm:gap-y-8 md:gap-7 lg:gap-6 xl:gap-x-4 xl:gap-y-6 justify-items-center'>
-                                    <Suspense fallback={<ProductsCardSkeleton />}>
-                                        <ProductWrapper page={Number(page)} category={category.toString()} />
-                                    </Suspense>
-                                </div>
+                                <Suspense fallback={<ProductsCardSkeleton />}>
+                                    <ProductWrapper page={Number(page)} category={category.toString()} sortBy={sortBy.toString()} sortDir={sortDir.toString()} search={search?.toString()} />
+                                </Suspense>
                             </div>
                         </div>
                         <div className='col-span-1'></div>
