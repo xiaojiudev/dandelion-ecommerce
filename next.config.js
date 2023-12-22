7069b4cf22b52b61b458287fc359ce39c6ec36e2
ldphat99 <ldphat99@gmail.com>
2023-12-22 23:26:27 +0700

    UPDATE: env in next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
     },
     reactStrictMode: false,
     env: {
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
         NEXT_PUBLIC_API_URI: process.env.NEXT_PUBLIC_API_URI,
         BACKEND_PUBLIC_API_URI: process.env.BACKEND_PUBLIC_API_URI,
     },
