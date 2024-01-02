59c14a743038ab140d67a4eb7893a664fa072714
ldphat99 <ldphat99@gmail.com>
2024-01-02 11:22:51 +0700

    DELETED

--- hooks/use-store-voucher.tsx
+++ hooks/use-store-voucher.tsx
@@ -1,13 +1,2 @@
 import { create } from 'zustand'
 
-interface useStoreVoucher {
-    isDisable: boolean
-    setEnable: () => void
-    setDisable: () => void
-}
-
-export const useStoreVoucher = create<useStoreVoucher>((set) => ({
-    isDisable: true,
-    setEnable: () => set({ isDisable: false }),
-    setDisable: () => set({ isDisable: true }),
-}))
